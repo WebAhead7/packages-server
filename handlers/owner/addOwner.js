f433c4c5ad8a07283f71747339a6a0d95edf83ec
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-28 20:42:22 +0200

    trynow

--- handlers/owner/addOwner.js
+++ handlers/owner/addOwner.js
@@ -10,19 +10,7 @@ const addOwner = async (req, res, next) => {
     idImage: req.body.idImage,
     businessId: req.body.businessId,
     payment: req.body.payment,
-    
   });
-  
-  firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
-    phone: { type: Number, min: 10 },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    phone: { type: Number, min: 8 },
-    idImage: { type: String, required: true },
-    businessId: { type: Number, min: 8, required: true },
-    address: { type: AddressSchema, required: true },
-    payment: { type: PaymentSchema, required: true },
 
   try {
     const newOwner = await owner.save();

ea85b67a51b4c20aab2497f3fb890d38af3b128b
OmklthomAmara <amaraomklthom@gmail.com>
2020-12-28 15:36:48 +0200

    business schema

--- db/db.js
+++ db/db.js
@@ -3,3 +3,15 @@ const express = require("express");
 const app = express();
 
 const { Schema } = mongoose;
+
+const Business = new Schema({
+  storeId: { type: String, required: true },
+  category: { type: [String], required: true },
+  items: { type: [Item] },
+  clients: { type: [Client] },
+  phone: { type: Number, required: true },
+  fax: { type: Number, required: true },
+  email: { type: String, required: true },
+  about: { type: String, required: true },
+  address: { type: Address, required: true },
+});

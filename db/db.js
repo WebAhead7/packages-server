d048da11daad8740e9b7f98c14ba7f5d244b8539
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-28 19:30:33 +0200

    new changes

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const app = express();
 
 const { Schema } = mongoose;
 
-const Address = new Schema({
+const AddressSchema = new Schema({
   city: { type: String, required: true },
   street: { type: String, required: true },
   building: { type: String, required: true },
@@ -14,101 +14,109 @@ const Address = new Schema({
   latitude: { type: String, required: true },
 });
 
-const Payment = new Schema({
-  creditNo: { type: Number, min: 16, required: true },
-  expDate: { type: String, required: true },
-  cvv: { type: Number, min: 3, required: true },
-});
+const PaymentSchema = new Schema(
+  {
+    creditNo: { type: Number, min: 16, required: true },
+    expDate: { type: String, required: true },
+    cvv: { type: Number, min: 3, required: true },
+  },
+  { timestamps: true }
+);
 
-const Bank = new Schema({
+const BankSchema = new Schema({
   bank_name: { type: String, required: true },
   bank_no: { type: Number, required: true },
   account_no: { type: String, required: true },
 });
 
-const Hours = new Schema({
+const HoursSchema = new Schema({
   day: { type: String, required: true },
   open: { type: String, required: true },
   close: { type: String, required: true },
 });
 
-const Package = new Schema({
-  name: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  id: { type: Number, required: true },
-  mid: { type: String, required: true },
-  weight: { type: Number, required: true },
-  delivery_price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
-  status: { type: String, required: true },
-  track_number: { type: String, required: true },
-  businessId: { type: String, required: true },
-  clientId: { type: String, required: true },
-  agentId: { type: String, required: true },
-});
+const PackageSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    id: { type: Number, required: true },
+    mid: { type: String, required: true },
+    weight: { type: Number, required: true },
+    delivery_price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    status: { type: String, required: true },
+    track_number: { type: String, required: true },
+    businessId: { type: String, required: true },
+    clientId: { type: String, required: true },
+    agentId: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const Client = new Schema({
-  name: { type: String, required: true },
-  id: { type: Number, required: true },
-  address: { type: Address, required: true },
-  phone: { type: Number, required: true },
-  email: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const ClientSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    id: { type: Number, required: true },
+    address: { type: AddressSchema, required: true },
+    phone: { type: Number, required: true },
+    email: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const business = new Schema({
+const businessSchema = new Schema({
   name: { type: String, required: true },
   storeId: { type: String, required: true },
   category: { type: [String], required: true },
-  items: { type: [Package] },
-  clients: { type: [Client] },
+  items: { type: [PackageSchema] },
+  clients: { type: [ClientSchema] },
   phone: { type: Number, required: true },
   fax: { type: Number, required: true },
   email: { type: String, required: true },
   about: { type: String, required: true },
-  address: { type: Address, required: true },
+  address: { type: AddressSchema, required: true },
   // hours: { type: Hours, required: true },
-  createdAt: { type: Date, default: Date.now },
 });
 
-const Business = mongoose.model("Business", business);
+const agentSchema = new Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    city: { type: String, required: true },
+    vehicle_type: { type: String, required: true },
+    vehicle_no: { type: Number, required: true },
+    id_num: { type: Number, required: true },
+    licenseId: { type: Number, required: true },
+    licenseImage: { type: String, required: true },
+    licenseDate: { type: Date, required: true },
+    rating: { type: Number, required: true },
+    Bank: { type: BankSchema, required: true },
+    monthly_paychecks: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
 
-const agent = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  city: { type: String, required: true },
-  vehicle_type: { type: String, required: true },
-  vehicle_no: { type: Number, required: true },
-  id_num: { type: Number, required: true },
-  licenseId: { type: Number, required: true },
-  licenseImage: { type: String, required: true },
-  licenseDate: { type: Date, required: true },
-  rating: { type: Number, required: true },
-  Bank: { type: Bank, required: true },
-  monthly_paychecks: { type: Boolean, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const shopOwnerSchema = new Schema(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    phone: { type: Number, min: 10 },
+    city: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phone: { type: Number, min: 8 },
+    idImage: { type: String, required: true },
+    businessId: { type: Number, min: 8, required: true },
+    address: { type: AddressSchema, required: true },
+    payment: { type: PaymentSchema, required: true },
+  },
+  { timestamps: true }
+);
 
-const Agent = mongoose.model("Agent", agent);
-
-const shopOwner = new Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  phone: { type: Number, min: 10 },
-  city: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  phone: { type: Number, min: 8 },
-  idImage: { type: String, required: true },
-  businessId: { type: Number, min: 8, required: true },
-  address: { type: Address, required: true },
-  payment: { type: Payment, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-const Shopowner = mongoose.model("Owner", shopOwner);
+const Business = mongoose.model("Business", businessSchema);
+const Agent = mongoose.model("Agent", agentSchema);
+const Shopowner = mongoose.model("Owner", shopOwnerSchema);
 
 module.exports = { Shopowner, Agent, Business };

1bed1c386ed2d3b1f231e63429a841d77185d49c
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2021-01-06 15:12:25 +0200

    agent flow done

--- handlers/agent/getAgentMiddleware.js
+++ handlers/agent/getAgentMiddleware.js
@@ -1,11 +1,12 @@
 const Agent = require("../../db/agent");
 
 const getAgent = async (req, res, next) => {
-  const agent_id = res.agent._id;
+  const agent = res.agent.agent;
+  console.log(agent._id);
   let agentById;
   try {
-    agentById = await Agent.findById(agent_id).exec();
-    if (agentById == null) {
+    agentById = await Agent.findById(agent._id).exec();
+    if (agentById == null || agentById.length === 0) {
       const error = new Error("Cannot find agent");
       error.status = 404;
       return next(error);
